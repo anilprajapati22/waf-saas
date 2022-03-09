9c020a951903465477d84f45ecc258e24dafc7d3
anilprajapati <anilprajapati18@gnu.ac.in>
2022-03-10 00:13:08 +0530

    model updated

--- gui/models.py
+++ gui/models.py
@@ -19,7 +19,7 @@ class wafdetails(models.Model):
     container_port = models.CharField(max_length=200)
     container_ip = models.CharField(max_length=200)
     public_ip = models.CharField(max_length=200)
-
+    project_name = models.CharField(max_length=200)
 
 class ons(models.Model):
     question = models.ForeignKey(sgn, on_delete=models.CASCADE)
